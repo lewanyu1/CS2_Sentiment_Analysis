ca06cbfac07f15b07accd275174f712ccc93c512
Ruler <Rulerwxe@outlook.com>
2025-12-16 22:25:11 +0800

    第二天：爬取模块，主代码未写完，setting完成

--- 1_spider_service/cs_spider/cs_spider/settings.py
+++ 1_spider_service/cs_spider/cs_spider/settings.py
@@ -19,12 +19,12 @@ ADDONS = {}
 #USER_AGENT = "cs_spider (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Concurrency and throttling settings
 #CONCURRENT_REQUESTS = 16
 CONCURRENT_REQUESTS_PER_DOMAIN = 1
-DOWNLOAD_DELAY = 1
+DOWNLOAD_DELAY = 5
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
@@ -46,9 +46,9 @@ DOWNLOAD_DELAY = 1
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "cs_spider.middlewares.CsSpiderDownloaderMiddleware": 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   "cs_spider.middlewares.CsSpiderDownloaderMiddleware": 543,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
@@ -58,9 +58,9 @@ DOWNLOAD_DELAY = 1
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "cs_spider.pipelines.CsSpiderPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "cs_spider.pipelines.CsSpiderPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
@@ -84,4 +84,75 @@ DOWNLOAD_DELAY = 1
 #HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
 
 # Set settings whose default value is deprecated to a future-proof value
-FEED_EXPORT_ENCODING = "utf-8"
+# 1. 确保这一行是 False
+
+## 3. 设置 User-Agent (使用你提供的 Mac 版本)
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36'
+
+# 4. 完整的请求头 (直接复制你提供的)
+DEFAULT_REQUEST_HEADERS = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-language": "zh-CN,zh;q=0.9",
+    "cache-control": "max-age=0",
+    "connection": "keep-alive",
+    "cookie": "BAIDUID_BFESS=F3B87751835B41E3E7A843F158C032F7:FG=1; BDUSS=X41LTN1ckhTYkd5cTV3U1JORDhxY1poaGppNjF4WGVRMjlCY05MTXRPYmdLVmxvSVFBQUFBJCQAAAAAAAAAAAEAAACa1qiwwNbA8cC0wPHN-QAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAOCcMWjgnDFoR; BDUSS_BFESS=X41LTN1ckhTYkd5cTV3U1JORDhxY1poaGppNjF4WGVRMjlCY05MTXRPYmdLVmxvSVFBQUFBJCQAAAAAAAAAAAEAAACa1qiwwNbA8cC0wPHN-QAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAOCcMWjgnDFoR; BAIDU_WISE_UID=wapp_1748098094696_836; Hm_lvt_292b2e1608b0823c1cb6beef7243ef34=1753194049; BIDUPSID=F3B87751835B41E3E7A843F158C032F7; PSTM=1754492969; H_WISE_SIDS=60279_62327_63143_63325_63881_63947_64009_64128_64142_64165_64173_64183_64215_64245_64247_64254_64258_64261_64306_64271_64317_64358_64365_64362_64364; STOKEN=0b6ccd8816a9962410f794139996da9fb5532563fd273293344201d2541081b2; wise_device=0; USER_JUMP=-1; 2963855002_FRSVideoUploadTip=1; video_bubble2963855002=1; BDRCVFR[abe9uUBlp-C]=mbxnW11j9Dfmh7GuZR8mvqV; H_PS_PSSID=60279_63143_66220_66242_66381_66291_66393_66529_66588_66580_66594_66604_66654_66678_66669_66718_66745_66616_66771_66787_66801_66803_66827_66850_66599_66841_66605; arialoadData=false; ZFY=ntwMAAFbXAqn5zLWZa8:B8p8Ic44Z:AOlL0OJCnkeb19M:C; TIEBA_SID=H4sIAAAAAAAAAzO0tDQwNo83BAATZ608CAAAAA; __ymg_scsc=1_60964e493333f0aea81dc5e08a4ce2044025cedf_0694167e1; XFI=b6becb50-da88-11f0-a0d3-4d8ab11bae51; BA_HECTOR=85ak050l008k8kah850l202k2g2k241kk2pv525; ariaappid=c890648bf4dd00d05eb9751dd0548c30; ariauseGraymode=false; XFCS=5EDD02EEE8045E0D4F14EBBEC779202A3AB5D04373A60F35E7D9F22846F3DA37; XFT=Hs4NIZjwg0Y2ItOmse1TEWbkIu9tQN+Owt7nGSZ4VX4=",
+    "host": "tieba.baidu.com",
+    "referer": "https://tieba.baidu.com/f?ie=utf-8&kw=csgo&fr=search",
+    "sec-ch-ua": '"Google Chrome";v="143", "Chromium";v="143", "Not A(Brand";v="24"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"macOS"',
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"}
+COOKIES_ENABLED = False
+
+
+# --- 8. 编码 ---
+FEED_EXPORT_ENCODING = 'utf-8'
+FEEDS = {
+    # 键(Key)：填您指定的完整路径（必须带文件名，比如 .csv）
+    '/home/rulerwxe/Code/pycharm/CS2_Sentiment_Analysis/2_data_warehouse/raw_data/tieba_result.csv': {
+
+        # 格式：存成 CSV 表格
+        'format': 'csv',
+
+        # 编码：必须是 utf-8，不然中文会变乱码
+        'encoding': 'utf-8',
+
+        # 覆盖模式：True = 每次运行把旧的删了存新的 (相当于 -O)
+        #           False = 每次运行接着往后写 (相当于 -o)
+        # 太奶您测试的时候，建议用 True，看着清爽
+        'overwrite': True,
+    }
+}
+
+
+
+
+
+# --- 4. 也是限制并发 (双重保险) ---
+CONCURRENT_REQUESTS_PER_IP = 1
+
+
+
+# 2. 开启【自动油门】(AutoThrottle)
+# 这是一个非常强大的功能，它会自动调整爬取速度
+AUTOTHROTTLE_ENABLED = True
+
+# 3. 起步速度 (5秒)
+AUTOTHROTTLE_START_DELAY = 5.0
+
+# 4. 最快也不能快过多少 (比如最快1秒，防止太快被封)
+# 如果您觉得太慢，可以改成 3.0，但建议稳一点
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+
+# 5. 开启调试模式 (方便您在日志里看到现在的真实速度)
+AUTOTHROTTLE_DEBUG = True
+
+# --- 其他配置保持不变 ---
+CONCURRENT_REQUESTS = 1
+RANDOMIZE_DOWNLOAD_DELAY = True
+CLOSESPIDER_ITEMCOUNT = 11000
--- 1_spider_service/cs_spider/cs_spider/spiders/tieba.py
+++ 1_spider_service/cs_spider/cs_spider/spiders/tieba.py
@@ -1,5 +1,15 @@
 import scrapy
 from cs_spider.items import CsSpiderItem
+from urllib.parse import urljoin
+import re
 
-def parse(self, response):
 
+class TiebaSpider(scrapy.Spider):
+    name = "tieba"
+    allowed_domains = ["baidu.com"]
+    start_urls = ["https://tieba.baidu.com/f?ie=utf-8&kw=csgo&pn=0"]
+
+    def parse(self, response):
+
+    def parse_detail(self, response):
+        
